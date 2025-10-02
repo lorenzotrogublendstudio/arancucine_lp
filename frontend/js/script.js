ff611853e552f2b557bcc6570f3a84b53f577bcb
lorenzotrogublendstudio <lorenzo.blenstudio@gmail.com>
2025-10-02 13:32:24 +0200

    meglio il frontend proviamo deploy mobile

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,6 +1,37 @@
 // Gestione semplice del form
-document.getElementById("contactForm").addEventListener("submit", function(e) {
+/*document.getElementById("contactForm").addEventListener("submit", function(e) {
   e.preventDefault();
   alert("Grazie per averci contattato! Ti risponderemo al più presto.");
   this.reset();
+});*/
+
+const modal = document.getElementById('modalForm');
+const closeModal = document.getElementById('closeModal');
+
+// Seleziona tutti i bottoni che aprono la modale
+const openButtons = document.querySelectorAll('.openModalBtn');
+
+openButtons.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+  });
+});
+
+closeModal.addEventListener('click', () => {
+  modal.classList.add('hide');
+  setTimeout(() => {
+    modal.classList.remove('show');
+  }, 400);
+});
+
+// Chiudi cliccando fuori dal contenuto
+window.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.classList.add('hide');
+    setTimeout(() => {
+      modal.classList.remove('show');
+    }, 400);
+  }
 });
